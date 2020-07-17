0933acfbe3f27ab4ba3ddee03dfb9ab74c4e8c6b
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2020-07-17 16:30:14 -0300

    Finalizada Parte 1

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-import { sumar, restar, multiplicar, dividir } from '../index.js';
+import { sumar, restar, multiplicar, dividir, isFalse, isNull, isTrue, isUndefined, arrDias, arrProvincias, objExpReg } from '../index.js';
 
 describe('Operaciones matemáticas', () => {
     test('Realizamos la suma', () => {
@@ -14,3 +14,80 @@ describe('Operaciones matemáticas', () => {
         expect(dividir(1,1)).toBe(1);
     });
 });
+describe('Common matchers', () => {
+    const datos = {
+        nombre: 'Persona 1',
+        edad: 10
+    }
+    const datos2 = {
+        nombre: 'Persona 1',
+        edad: 10
+    }
+    test('Comprobamos que los objectos son iguales', () => {
+        expect(datos).toEqual(datos2);
+    });
+});
+describe('Numeric matchers', () => {
+    test('Resultado menor que...', () => {
+        expect(restar(5,3)).toBeLessThan(3);
+    });
+    test('Resultado menor o igual que...', () => {
+        expect(restar(5,3)).toBeLessThanOrEqual(2);
+    });
+    test('Resultado mayor o igual que...', () => {
+        expect(multiplicar(2,5)).toBeGreaterThanOrEqual(10);
+    });
+    test('Resultado mayor que...', () => {
+        expect(sumar(5,5)).toBeGreaterThan(9);
+    });
+});
+describe('Matchers Boolean, Undefined o Null', () => {
+    test('Resultado true', () => {
+        expect(isTrue()).toBeTruthy();
+    });
+    test('Resultado false', () => {
+        expect(isFalse()).toBeFalsy();
+    });
+    test('Resultado undefined', () => {
+        expect(isUndefined()).toBeUndefined();
+    });
+    test('Resultado null', () => {
+        expect(isNull()).toBeNull();
+    });
+});
+describe('Matchers Arrays', () => { 
+    test('Madrid existe en el array', () => {
+        expect(arrProvincias()).toContain('Madrid');
+    });
+    test('Guadalajara no existe en el array', () => {
+        expect(arrProvincias()).not.toContain('Guadalajara');
+    });
+    test('El array semana tiene 9 elementos', () => {
+        expect(arrProvincias()).toHaveLength(9);
+    });
+    test('Existe Lunes en el array semana', () => {
+        expect(arrDias()).toContain('Lunes');
+    });
+});
+describe('Matchers Strings', () => {
+    const exp = objExpReg();
+    test('Comprobamos si la respuesta es correcta', () => {
+        expect(exp.responseOK).toMatch(/OK/);
+    });
+    test('Comprobamos si la respuesta es incorrecta', () => {
+        expect(exp.responseFAIL).toMatch(/FAIL/);
+    });
+    test('Comprobamos si la respuesta tiene una longitud', () => {
+        expect(exp.responseFAIL).toHaveLength(13);
+    });
+    test('Comprobamos dirección de email', () => {
+        expect(exp.email).toMatch(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/);
+    })
+    test('Comprobamos número de teléfono', () => {
+        expect(exp.telefono).toMatch(/^[9|6|7][0-9]{8}$/);
+    });
+});
+afterEach(() => console.log('Despues de cada prueba'));
+afterAll(() => console.log("Despues de todas las pruebas"));
+beforeEach(() => console.log('Antes de cada prueba'));
+beforeAll(() => console.log('Antes de todas las pruebas'));
